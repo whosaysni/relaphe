7429d8b65726741dafb5a58489fd8bd4199cedb7
Yasushi MASUDA <whosaysni@gmail.com>
2022-04-16 21:06:39 +0900

    add modulo-11/16/43/47 support

--- relaphe/util/modulo.py
+++ relaphe/util/modulo.py
@@ -1,27 +1,47 @@
 # Modulo checksum algorithms
-from typing import Generator
+from itertools import cycle
+from typing import List
+
+
+MAX_DIGITS = 128  # prevents endless input digits
 
 
 class Modulo:
     """Modulo algorithm
 
-    >>> code25_checksum = Modulo(cycle([1, 3]), 10)
-    >>> code25_checksum([1,2,3,4,5])
+    >>> weights = [w for i, w in zip(cycle([1, 3]), range(5))]
+    >>> code25_checksum = Modulo(weights, 10)
+    >>> code25_checksum([1, 2, 3, 4, 5])
     7
     """
 
-    def __init__(self, wgen: Generator, mod: int):
+    def __init__(self, weights: List[int], divisor: int, use_subtract=True):
         """Creates modulo checksum function
 
-        :param wgen: weight sequence generator
-        :param mod: modulator
-        """
-        self.wgen = wgen
-        self.mod = mod
+        Weights are repeated if shorter than input digits.
+        (ex. 1,3,1,3,1,3,1,3... can be expressed as [1,3].
 
-    def __call__(self, digits):
+        :param weights: Weight vector
+        :param divisor: Divisor for divmod operation
+        :param subtract: If true, use subtract from divisor instead of remains
+        """
+        self.weights = weights
+        self.divisor = divisor
+        self.use_subtract = use_subtract
+
+    def weighted_sum(self, digits: List[int]) -> int:
+        """Calculate weighted sum for given digits
+
+        :param digits: Input digit sequence
+        """
+        return sum(
+            d * w for d, w, i in zip(digits, cycle(self.weights), range(MAX_DIGITS))
+        )
+
+    def __call__(self, digits: List[int]):
         """Caluculate checksum
 
-        :param digits: input digit sequence
+        :param digits: Input digit sequence
         """
-        return self.mod - sum(d*w for d, w in zip(digits, self.wgen))%self.mod
+        remains = self.weighted_sum(digits) % self.divisor
+        return (self.divisor - remains) % self.divisor if self.use_subtract else remains
--- tests/unit/test_util_modulo.py
+++ tests/unit/test_util_modulo.py
@@ -1,14 +1,34 @@
+import pytest
+from relaphe.util.modulo import Modulo
+from itertools import cycle
 
-def test_modulo():
-    from relaphe.util.modulo import Modulo
-    from itertools import cycle
-    modulo10_ean13 = Modulo(cycle([1, 3]), 10)
-    modulo10_ean14 = Modulo(cycle([3, 1]), 10)
-    modulo10_code25 = Modulo(cycle([3, 1]), 10)
-    modulo10_leitcode = modulo10_identcode = Modulo(cycle([4, 9]), 10)
-    assert modulo10_ean13([4,0,0,7,6,3,0,0,0,0,1,1]) == 6
-    assert modulo10_ean14([0,4,0,0,7,6,3,0,0,0,1,1]) == 6
-    assert modulo10_code25([1,2,3,4,5]) == 7
-    assert modulo10_leitcode([2,3,6,6,9,0,1,2,0,1,2,3,0]) == 5
+d = lambda s: [int(c) for c in s]
 
-    
+
+modulo_cases = [
+    # label, weights, divisor, use_subtract, digits, result
+    # modulo 10
+    ('ean13', [1, 3], 10, True, d('400763000011'), 6),
+    ('ean14', [3, 1], 10, True, d('0400763000011'), 6),
+    ('code25', [3, 1], 10, True, d('12345'), 7),
+    ('leitcode', [4, 9], 10, True, d('2366901201230'), 5),
+    # modulo 11
+    ('pzn', [2,3,4,5,6,7], 11, False, d('631942'), 9),
+    ('isbn10', [10, 9, 8, 7, 6, 5, 4, 3, 2], 11, True, d('392844404'), 2),
+    ('isbn10', [10, 9, 8, 7, 6, 5, 4, 3, 2], 11, True, d('392844400'), 10),
+    # modulo 16
+    ('codabar', [1], 16, True, [16, 7, 8, 9, 16], 8),
+    # modulo 43
+    ('code39', [1], 43, False, [1, 5, 9, 10, 35], 17),
+    # modulo 47
+    ('code93', [7, 6, 5, 4, 3, 2, 1], 47, False, [12, 24, 13, 14, 38, 9, 3], 14),
+    ('code93e', [8, 7, 6, 5, 4, 3, 2, 1], 47, False, [12, 24, 13, 14, 38, 9, 3, 14], 0),
+]
+@pytest.mark.parametrize(
+    'label, weights, divisor, use_subtract, digits, result',
+    modulo_cases
+)
+def test_modulo(label, weights, divisor, use_subtract, digits, result):
+    checksum_func = Modulo(weights, divisor, use_subtract)
+    assert checksum_func(digits) == result
+
